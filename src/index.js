a51c8f7ea36d6470141fd53191d5642f2dc3862e
Joshua Osunlakin <joshuaolaoluwa7@gmail.com>
2021-02-06 05:20:41 +0100

    complete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // complete the function
 function palindrom(str) {
   // code goes here
+  var something = str.split("").reverse().join("")
+  if(something===str)
+    return true;
+  return false;
+  
 }
 
 function solution(arg) {
   return palindrom(arg);
 }
-
+ 
 if (typeof require !== "undefined" && require.main === module) {
   if (process.argv.length <= 2) {
     throw Error("Require argument");
